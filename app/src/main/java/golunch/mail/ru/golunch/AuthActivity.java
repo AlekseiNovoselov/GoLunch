39ac1f8424d2ac79c66e832a2334885eb08de76e
aleksei <aleksei.novoselov@corp.mail.ru>
2016-11-11 01:23:28 +0300

    "Получаю токен"

--- app/src/main/java/golunch/mail/ru/golunch/AuthActivity.java
+++ app/src/main/java/golunch/mail/ru/golunch/AuthActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -19,6 +20,8 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GetTokenResult;
 import com.google.firebase.auth.GoogleAuthProvider;
 
 import golunch.mail.ru.golunch.helper.GoogleAuthActivity;
@@ -34,6 +37,22 @@ public class AuthActivity extends GoogleAuthActivity {
     protected void onMyAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
         if (firebaseAuth.getCurrentUser() != null) {
             startActivity(new Intent(AuthActivity.this, MainActivity.class));
+            FirebaseUser mUser = FirebaseAuth.getInstance().getCurrentUser();
+            mUser.getToken(true)
+                    .addOnCompleteListener(new OnCompleteListener<GetTokenResult>() {
+                        public void onComplete(@NonNull Task<GetTokenResult> task) {
+                            if (task.isSuccessful()) {
+                                String idToken = task.getResult().getToken();
+                                Log.e("Lexalog", "Token:" + idToken);
+                                // Send token to your backend via HTTPS
+                                // ...
+                            } else {
+                                Log.e("Lexalog", "Error");
+                                // Handle error -> task.getException();
+                            }
+                        }
+                    });
+
         }
     }
 
@@ -70,7 +89,6 @@ public class AuthActivity extends GoogleAuthActivity {
 
             }
         });
-
     }
 
     private void signIn() {
